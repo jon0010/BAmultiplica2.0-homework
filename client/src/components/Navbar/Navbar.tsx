a9a4aa999714acec99a27e06bf50aa8648b3068d
Jon Nahuel Pereyra <jonnahuel78@gmail.com>
2023-08-31 16:46:13 -0300

    prueba 2 deploy

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -29,7 +29,7 @@ const Navbar = () => {
                 aria-current="page"
                 to="/semanauno"
               >
-                SEMANA aaaaa
+                SEMANA bbb
               </Link>
             </li>
             <li className="nav-item">
