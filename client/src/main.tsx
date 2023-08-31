52994321755f188df759697961e481461f17b294
Jon Nahuel Pereyra <jonnahuel78@gmail.com>
2023-08-31 16:48:50 -0300

    prueba 3 deploy

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,6 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import React from "react";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <BrowserRouter>
